2e2d8df54dc418320a0a53082283c782f9a0ae17
ajloveslily14 <ajloveslily@gmail.com>
2019-07-15 21:05:05 -0700

    add eval command

---
+++ commands/eval.lua
@@ -0,0 +1,30 @@
+local function eval(msg,args)
+
+	if msg.author ~= client.owner then reutrn end
+	local env = getfenv()
+	env.m = m
+	env.send = function(...)
+		local t = {}
+		if #{...} == 0 then 
+			t = {"nil"}
+		else
+			for k,v in pairs({...}) do
+				table.insert(t,tostring(v))
+			end
+		end
+		m:reply(table.concat(t,"\t"))
+	end
+
+	local fn,err = load(code,"Eval","t",env)
+	if not fn then
+		m:reply("There was a syntax error: ```lua\n"..err.."```")
+		return
+	end
+
+	local ret,err2 = pcall(fn)
+	if not ret then
+		m:reply("There was a runtime error: ```lua\n"..err2.."```")
+	end
+end
+
+addCommand("geval","Run lua code",eval)
