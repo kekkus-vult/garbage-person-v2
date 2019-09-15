15a9b8b7eac8c3998981a3a6ed53c3213eb71d81
ajloveslily14 <ajloveslily@gmail.com>
2019-09-15 00:18:09 -0700

    add pattern support for lyrics.lua
    lets you be more flexable with punctuation

--- modules/lyrics.lua
+++ modules/lyrics.lua
@@ -7,22 +7,25 @@ triggers["son when"] = "you grow up"
 triggers["will you be"] = "the savior of the broken"
 
 
-triggers["what does marsellus wallace look like"] = "what?"
-triggers["what country are you from"] = "what?"
-triggers["what aint no country ive ever heard of. they speak english in what?"] = "what?"
-triggers["english motherfucker do you speak it"] = "yes! yes!"
-triggers["then you know what im sayin"] = "yes!"
+triggers["what does marsellus wallace look like%??"] = "what?"
+triggers["what country are you from%??"] = "what?"
+triggers["what ain'?t no country i'?ve ever heard of.? they speak english in what%??"] = "what?"
+triggers["english mother ?fucker do you speak it[!?]?[!?]?"] = "yes! yes!"
+triggers["then you know what i'?m sayin'?%??"] = "yes!"
 triggers["describe what marsellus wallace looks like"] = "what"
-triggers["say what again. say what again i dare you i double dare you motherfucker say what one more goddamn time"] = "hes black"
-triggers["go on!"] = "hes bald"
-triggers["does he look like a bitch"] = "what?! **gunshot**"
+triggers["say what again[!.]? say what again i dare you i double dare you mother ?fucker say what one more goddamn time!?"] = "hes black"
+triggers["go on!?"] = "hes bald"
+triggers["does he look like a bitch[!?]?[!?]?"] = "what?! **gunshot**"
 
 
 local function setHook()
 	return client:on("messageCreate",function(m)
 		if m.author == client.user then return end
-		if triggers[string.lower(m.content)] then
-			m:reply(triggers[string.lower(m.content)])
+		if not m.content then return end
+		for trigger,reply in pairs(triggers) do
+			if m.content:lower():find("^"..trigger.."$") then
+				return m:reply(reply)
+			end
 		end
 	end)
 end
